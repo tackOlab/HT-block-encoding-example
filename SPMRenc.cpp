2bcbbd408e65d2cc70c24bac7a5a0be4028993a7
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2022-01-13 17:23:43 +0900

    finish

--- SPMRenc.cpp
+++ SPMRenc.cpp
@@ -28,18 +28,19 @@ void j2k_codeblock::set_MagSgn_and_sigma(uint32_t &or_val) {
       //   temp += sign >> 31;
       dp[j] = temp;
       // }
-      if (sign) {
-        printf("-%d ", dp[j] & 0x7fffffff);
-      } else {
-        printf(" %d ", dp[j] & 0x7fffffff);
-      }
+
+      // if (sign) {
+      //   printf("-%d ", dp[j] & 0x7fffffff);
+      // } else {
+      //   printf(" %d ", dp[j] & 0x7fffffff);
+      // }
       block_index++;
     }
-    printf("\n");
+    // printf("\n");
   }
 }
 
-auto process_stripes_block = [](SP_dec &SigProp, j2k_codeblock *block, const uint16_t i_start,
+auto process_stripes_block = [](SP_enc &SigProp, j2k_codeblock *block, const uint16_t i_start,
                                 const uint16_t j_start, const uint16_t width, const uint16_t height,
                                 const uint16_t dum_stride, const uint8_t &pLSB) {
   int32_t *sp;
@@ -59,30 +60,28 @@ auto process_stripes_block = [](SP_dec &SigProp, j2k_codeblock *block, const uin
       }
       mbr_info >>= 3;
       if (mbr != 0) {
+        bit = *sp & (1 << pLSB);  // HT Cleanupの設定によってpLSBは変化する可能性がある（当分0）
+        SigProp.emitSPBit(bit);
         block->modify_state(refinement_indicator, 1, i, j);
-        bit = SigProp.importSigPropBit();
         block->modify_state(refinement_value, bit, i, j);
-        // block->set_refinement_value(bit, i, j);
-        *sp |= bit << pLSB;
       }
       block->modify_state(scan, 1, i, j);
-      // block->update_scan_state(1, i, j);
     }
   }
   for (uint16_t j = j_start; j < j_start + width; j++) {
     for (uint16_t i = i_start; i < i_start + height; i++) {
       sp = &block->sample_buf[j + i * block->size.x];
-      // decode sign
-      if ((*sp & (1 << pLSB)) != 0) {
-        *sp = (*sp & 0x7FFFFFFF) | (SigProp.importSigPropBit() << 31);
+      // encode sign
+      // if ((((*sp & 0x7fffffff) & (1 << pLSB)) != 0) & (block->get_state(Refinement_indicator, i, j))) {
+      if (block->get_state(Refinement_value, i, j)) {
+        bit = (*sp < 0);  // sp < 0 なら bit = 1 となる
+        SigProp.emitSPBit(bit);
       }
     }
   }
 };
 
-void ht_sigprop_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_t magref_length,
-                       const uint8_t &pLSB) {
-  SP_dec SigProp(HT_magref_segment, magref_length);
+void ht_sigprop_encode(j2k_codeblock *block, SP_enc &SigProp, const uint8_t &pLSB) {
   const uint16_t num_v_stripe = block->size.y / 4;
   const uint16_t num_h_stripe = block->size.x / 4;
   uint16_t i_start            = 0, j_start;
@@ -91,7 +90,7 @@ void ht_sigprop_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_
   uint16_t height           = 4;
   const uint16_t dum_stride = block->size.x + 2;
 
-  // decode full-height (=4) stripes
+  // encode full-height (=4) stripes
   for (uint16_t n1 = 0; n1 < num_v_stripe; n1++) {
     j_start = 0;
     for (uint16_t n2 = 0; n2 < num_h_stripe; n2++) {
@@ -104,7 +103,7 @@ void ht_sigprop_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_
     }
     i_start += 4;
   }
-  // decode remaining height stripes
+  // encode remaining height stripes
   height  = block->size.y % 4;
   j_start = 0;
   for (uint16_t n2 = 0; n2 < num_h_stripe; n2++) {
@@ -117,23 +116,23 @@ void ht_sigprop_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_
   }
 }
 
-void ht_magref_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_t magref_length,
-                      const uint8_t &pLSB) {
-  MR_dec MagRef(HT_magref_segment, magref_length);
+void ht_magref_encode(j2k_codeblock *block, MR_enc &MagRef, const uint8_t &pLSB) {
   const uint16_t blk_height   = block->size.y;
   const uint16_t blk_width    = block->size.x;
   const uint16_t num_v_stripe = block->size.y / 4;
   uint16_t i_start            = 0;
   uint16_t height             = 4;
   int32_t *sp;
+  uint8_t bit;
 
   for (uint16_t n1 = 0; n1 < num_v_stripe; n1++) {
     for (uint16_t j = 0; j < blk_width; j++) {
       for (uint16_t i = i_start; i < i_start + height; i++) {
         sp = &block->sample_buf[j + i * block->size.x];
         if (block->get_state(Sigma, i, j) != 0) {
+          bit = sp[0] & (1 << pLSB);  // HT Cleanupの設定によってpLSBは変化する可能性がある（当分0）
+          MagRef.emitMRBit(bit);
           block->modify_state(refinement_indicator, 1, i, j);
-          sp[0] |= MagRef.importMagRefBit() << pLSB;
         }
       }
     }
@@ -144,8 +143,9 @@ void ht_magref_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_t
     for (uint16_t i = i_start; i < i_start + height; i++) {
       sp = &block->sample_buf[j + i * block->size.x];
       if (block->get_state(Sigma, i, j) != 0) {
+        bit = sp[0] & (1 << pLSB);  // HT Cleanupの設定によってpLSBは変化する可能性がある（当分0）
+        MagRef.emitMRBit(bit);
         block->modify_state(refinement_indicator, 1, i, j);
-        sp[0] |= MagRef.importMagRefBit() << pLSB;
       }
     }
   }
@@ -153,11 +153,34 @@ void ht_magref_decode(j2k_codeblock *block, uint8_t *HT_magref_segment, uint32_t
 
 int32_t htj2k_encode(j2k_codeblock *const block, const uint8_t ROIshift) noexcept {
   uint32_t or_val = 0;
+  // DWT係数をメンバ変数 sample_buf にコピー
   block->set_MagSgn_and_sigma(or_val);
+  // SigProp + MagRef <= 2047 byte という決まりがある
+  uint8_t Dref0[2047] = {0};
+  uint8_t Dref1[2047] = {0};
+  // エンコード状態のためのクラスを作成(initXXPacker)
+  SP_enc SigProp(Dref0);
+  MR_enc MagRef(Dref1);
+  // SigProp encoding
+  ht_sigprop_encode(block, SigProp, 0);
+  // MagRef encoding
+  ht_magref_encode(block, MagRef, 0);
+  // 終端処理
+  // MRのデータは左右反転してSPの後ろにくっつく
+  termSPandMR(SigProp, MagRef);
+
+  // 結果の表示
+  printf("SP length = %d\n", SigProp.getPos());
+  printf("MR length = %d\n", MagRef.getPos());
+  for (int i = 0; i < SigProp.getPos() + MagRef.getPos(); ++i) {
+    printf("%02X ", Dref0[i]);
+  }
+  printf("\n");
   return 0;
 }
 
 int main() {
+  // コードブロックのパラメータ設定（今回だけ）
   uint32_t idx            = 0;
   uint8_t orientation     = 0;
   uint8_t M_b             = 8;
@@ -172,12 +195,15 @@ int main() {
   element_siz p1(64, 64);
   element_siz s(64, 64);
 
+  // 量子化されたDWT係数を格納するバッファ領域の作成
   int32_t ibuf[4096];
   for (int i = 0; i < 4096; ++i) {
     ibuf[i] = (in_sign[i] << 31) | bitplane[i];
   }
+  // コードブロッククラスのインスタンスの作成
   j2k_codeblock block(idx, orientation, M_b, R_b, transformation, stepsize, band_stride, ibuf, offset,
                       numlayers, codeblock_style, p0, p1, s);
+  // エンコード処理
   htj2k_encode(&block, 0);
 
   return 0;
--- SPMRenc.h
+++ SPMRenc.h
@@ -2,6 +2,109 @@
 #include <cstdint>
 #include <cstdio>
 #include <exception>
+class MR_enc;  // forward declaration for friend function termSPandMR()
+/********************************************************************************
+ * SP_enc: state classe for HT SigProp encoding
+ *******************************************************************************/
+class SP_enc {
+ private:
+  uint32_t pos;
+  uint8_t bits;
+  uint8_t max;
+  uint8_t tmp;
+  uint8_t *const buf;
+  friend int32_t termSPandMR(SP_enc &, MR_enc &);
+
+ public:
+  SP_enc(uint8_t *Dref) : pos(0), bits(0), max(8), tmp(0), buf(Dref) {}
+  void emitSPBit(uint8_t bit) {
+    tmp |= (bit << bits);
+    bits++;
+    if (bits == max) {
+      buf[pos] = tmp;
+      pos++;
+      max  = (tmp == 0xFF) ? 7 : 8;
+      tmp  = 0;
+      bits = 0;
+    }
+  }
+  void termSP() {
+    if (tmp != 0) {
+      buf[pos] = tmp;
+      pos++;
+      max = (tmp == 0xFF) ? 7 : 8;
+    }
+    if (max == 7) {
+      buf[pos] = 0x00;
+      pos++;  // this prevents the appearance of a terminal 0xFF
+    }
+  }
+  uint32_t getPos() { return pos; }
+  void show() {
+    for (int i = 0; i < pos; ++i) {
+      printf("%02x ", buf[i]);
+    }
+    printf("\n");
+  }
+};
+/********************************************************************************
+ * MR_enc: state classe for HT MagRef encoding
+ *******************************************************************************/
+class MR_enc {
+ private:
+  uint32_t pos;
+  uint8_t bits;
+  uint8_t tmp;
+  uint8_t last;
+  uint8_t *const buf;
+  friend int32_t termSPandMR(SP_enc &, MR_enc &);
+
+ public:
+  MR_enc(uint8_t *Dref) : pos(0), bits(0), tmp(0), last(255), buf(Dref) {}
+  void emitMRBit(uint8_t bit) {
+    tmp |= (bit << bits);
+    bits++;
+    if ((last > 0x8F) && (tmp == 0x7F)) {
+      bits++;  // this must leave MR_bits equal to 8
+    }
+    if (bits == 8) {
+      buf[pos] = tmp;
+      pos++;
+      last = tmp;
+      tmp  = 0;
+      bits = 0;
+    }
+  }
+  uint32_t getPos() { return pos; }
+  void show() {
+    for (int i = 0; i < pos; ++i) {
+      printf("%02x ", buf[i]);
+    }
+    printf("\n");
+  }
+};
+
+int32_t termSPandMR(SP_enc &SP, MR_enc &MR) {
+  uint8_t SP_mask = 0xFF >> (8 - SP.bits);  // if SP_bits is 0, SP_mask = 0
+  SP_mask |= ((1 << SP.max) & 0x80);        // Auguments SP_mask to cover any stuff bit
+  uint8_t MR_mask = 0xFF >> (8 - MR.bits);  // if MR_bits is 0, MR_mask = 0
+  if ((SP_mask | MR_mask) == 0) {
+    return 0;  // last SP byte cannot be 0xFF, since then SP_max would be 7
+  }
+  uint8_t fuse = SP.tmp | MR.tmp;
+  if ((((fuse ^ SP.tmp) & SP_mask) | ((fuse ^ MR.tmp) & MR_mask)) == 0) {
+    SP.buf[SP.pos] = fuse;  // fuse always < 0x80 here; no false marker risk
+  } else {
+    SP.buf[SP.pos] = SP.tmp;  // SP_tmp cannot be 0xFF
+    MR.buf[MR.pos] = MR.tmp;
+    MR.pos++;
+  }
+  SP.pos++;
+  for (int i = MR.pos - 1, j = 0; i >= 0; --i, ++j) {
+    SP.buf[SP.pos + j] = MR.buf[i];
+  }
+  return 1;
+}
 /********************************************************************************
  * SP_dec: state classe for HT SigProp decoding
  *******************************************************************************/
